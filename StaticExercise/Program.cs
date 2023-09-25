6665e3638e5ccf0eedc66419893f75fb0e63f65c
mangosax <mcguerty@att.net>
2023-09-25 17:05:05 -0500

    So busy I forgot to push, LOL!

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+            Console.WriteLine("Do you want to convert");
+            Console.WriteLine("1 for Fahrenheit to Celsisus");
+            Console.WriteLine("2 for Celsisus to Fahrenheit");
+            string? tmpConvert = Console.ReadLine();
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Enter Temp:");
+            double tmpTemp = 0;
+            string? tmpStr = "";
+            tmpStr = Console.ReadLine();
+            tmpTemp = Convert.ToDouble(tmpStr);
+
+            if (tmpConvert == "1")
+            {
+                TempConverter.FahrenheitToCelsius(tmpTemp);
+            }
+            else if (tmpConvert == "2") 
+            {
+                TempConverter.CelsiusToFahrenheit(tmpTemp);
+            }
+        }//Main
+    }//Program
+}//Namespace
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace StaticExercise
+{
+    static class TempConverter
+    {
+
+
+
+
+    static public double FahrenheitToCelsius(double fTemp)
+        {
+            Console.WriteLine($"Converting {fTemp} to celsius");
+            Console.WriteLine($"{fTemp} = {(fTemp - 32) * (5 / 9)} celsius");
+            Console.WriteLine($"Press return to continue> ");
+            Console.ReadLine();
+            return (fTemp - 32) * (5 / 9);
+        }
+
+        static public double CelsiusToFahrenheit(double cTemp)
+        {
+            Console.WriteLine($"Converting {cTemp} to Fahrenheits");
+            Console.WriteLine($"{cTemp} = {(cTemp * 1.8) +32 } Fahrenheit ");
+            Console.WriteLine($"Press return to continue> ");
+            Console.ReadLine();
+            return   (cTemp * 1.8) + 32;
+        }
+
+
+    }
+}
